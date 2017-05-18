e28843b6dd73fd92e276d68a2aa204dd5948204c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-18 11:36:07 +0530

    Break up reducer into multiple reducers

--- app/redux-example.js
+++ app/redux-example.js
@@ -11,52 +11,58 @@ let stateDefault = {
 let nextHobbyId = 1
 let nextMovieId = 1
 
-let reducer = (state = stateDefault, action) => {
-  // state = state || {name: 'Anonymous'} // ES5 way of passing default parameters
-
+let nameReducer = (state = 'Anonymous', action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      }
-    case 'ADD_HOBBY':
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            hobby: action.hobby
-          }
-        ]
-      }
-    case 'REMOVE_HOBBY':
-      return {
-        ...state,
-        hobbies: state.hobbies.filter((hobby) => action.id !== hobby.id)
-      }
-    case 'ADD_MOVIE':
-      return {
-        ...state,
-        movies: [
-          ...state.movies,
-          {
-            id: nextMovieId++,
-            movie: action.movie,
-            genere: action.genere
-          }
-        ]
-      }
-    case 'REMOVE_MOVIE':
-      return {
-        ...state,
-        movies: state.movies.filter((movie) => action.id !== movie.id)
-      }
+      return action.name
     default:
-      return state // should always return state even if there is no action
+      return state
   }
 }
+
+let hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        {
+          hobby: action.hobby,
+          id: nextHobbyId++
+        }
+      ]
+    case 'REMOVE_HOBBY':
+      return state.filter((hobby) => {
+        return hobby.id !== action.id
+      })
+    default:
+      return state
+  }
+}
+
+let moviesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return [
+        ...state,
+        {
+          movie: action.movie,
+          id: nextMovieId++
+        }
+      ]
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => {
+        return action.id !== movie.id
+      })
+    default:
+      return state
+  }
+}
+
+let reducer = redux.combineReducers({
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer
+})
 
 let store = redux.createStore(reducer, redux.compose( // redux.compose let us add middleware functions
   window.devToolsExtension ? window.devToolsExtension() : f => f
